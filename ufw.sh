fe3cba435ab4175c6cb6177af0475031cd7be3e8
lotusnoir <lotus.noir@gmail.com>
2025-01-07 18:52:05 +0000

    fix scripts

--- ufw.sh
+++ ufw.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if [ -z "$USER" ] ; then
+    USER=$(whoami)
+fi
 if [ "$USER" != "root" ] ; then
     echo "$(basename $0) must be run as root!"
     exit 2
@@ -7,11 +10,19 @@ fi
 
 UFW_STATE=0
 UFW_EXIST=0
+PROBLEM_COUNT=0
 
 if [ -f /sbin/ufw ]; then
 	UFW_EXIST=1
 	UFW_STATE=$(ufw status | grep "Status: active" | wc -l)
+        if [ "$?" -ne "0" ] ; then
+            PROBLEM_COUNT=$((PROBLEM_COUNT + 1))
+        fi
 fi
+
+echo "# HELP ufw_check_problems Count problems encountered while checking ufw"
+echo "# TYPE ufw_check_problems gauge"
+echo ufw_check_problems $PROBLEM_COUNT
 
 echo "# HELP ufw_exist Check if ufw is installed"
 echo "# TYPE ufw_exist gauge"
@@ -20,3 +31,8 @@ echo ufw_exist $UFW_EXIST
 echo "# HELP ufw_up Check if ufw is running"
 echo "# TYPE ufw_up gauge"
 echo ufw_up $UFW_STATE
+
+if [ "$PROBLEM_COUNT" -ne "0" ] ; then
+    exit 1
+fi
+exit 0
--- wrong_disk_on_boot.sh
+++ wrong_disk_on_boot.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [ -z "$USER" ] ; then
+    USER=$(whoami)
+fi
 if [ "$USER" != "root" ] ; then
     echo "$(basename $0) must be run as root!"
     exit 2
 fi
 
+PROBLEM_COUNT=0
 PRIMARY_DISK_CODE=0
 PRIMARY_DISK=$(df | awk '$NF == "/" || $NF == "/boot" { print $1}' | grep -E "da[0-9]" | wc -l)
+if [ "$?" -ne "0" ] ; then
+    PROBLEM_COUNT=$((PROBLEM_COUNT + 1))
+fi
 
 if [ "$PRIMARY_DISK" -eq 0 ]; then
     PRIMARY_DISK_CODE=1
@@ -15,3 +22,8 @@ fi
 echo "# HELP node_wrong_disk_on_boot Check if a letter on boot disk"
 echo "# TYPE node_wrong_disk_on_boot gauge"
 echo node_wrong_disk_on_boot $PRIMARY_DISK_CODE
+
+if [ "$PROBLEM_COUNT" -ne "0" ] ; then
+    exit 1
+fi
+exit 0
