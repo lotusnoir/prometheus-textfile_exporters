b60ae70c170757f15fe02e8b7d38695c3a7a105d
lotusnoir <lotus.noir@gmail.com>
2025-01-09 14:13:48 +0000

    variable to skip internet check

--- exporter_version_check.sh
+++ exporter_version_check.sh
@@ -15,6 +15,7 @@ fi
 # Set global variables
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 PROBLEM_COUNT=0
+LATEST_CHECK=0
 
 #######################################
 ### Checks
@@ -25,8 +26,10 @@ NODE_VERSION_LATEST=0
 if [ -f /usr/local/bin/node_exporter ]; then
 	NODE_VERSION=$(/usr/local/bin/node_exporter --version | head -1| awk '{print $3}')
         if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
-	NODE_VERSION_LATEST=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
-        if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	if [ "$LATEST_CHECK" -eq "1" ] ; then
+		NODE_VERSION_LATEST=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
+		if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	fi
 fi
 echo "# HELP node_exporter_version Check node_exporter binary version"
 echo "# TYPE node_exporter_version gauge"
@@ -41,8 +44,10 @@ CHRONY_VERSION_LATEST=0
 if [ -f /usr/local/bin/node_exporter ]; then
 	CHRONY_VERSION=$(/usr/local/bin/chrony_exporter --version | head -1| awk '{print $3}')
         if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
-	CHRONY_VERSION_LATEST=$(curl -s https://api.github.com/repos/SuperQ/chrony_exporter/releases/latest| grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
-        if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	if [ "$LATEST_CHECK" -eq "1" ] ; then
+		CHRONY_VERSION_LATEST=$(curl -s https://api.github.com/repos/SuperQ/chrony_exporter/releases/latest| grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
+		if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	fi
 fi
 echo "# HELP chrony_exporter_version Check chrony_exporter binary version"
 echo "# TYPE chrony_exporter_version gauge"
@@ -58,8 +63,10 @@ if [ -f /usr/local/bin/conntrack_exporter ]; then
 	#CONNTRACK_VERSION=$(/usr/local/bin/conntrack_exporter --version | head -1| awk '{print $3}')
 	CONNTRACK_VERSION="0.3.1"
         #if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
-	CONNTRACK_VERSION_LATEST=$(curl -s https://api.github.com/repos/hiveco/conntrack_exporter/releases/latest | grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
-        if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	if [ "$LATEST_CHECK" -eq "1" ] ; then
+		CONNTRACK_VERSION_LATEST=$(curl -s https://api.github.com/repos/hiveco/conntrack_exporter/releases/latest | grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
+		if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	fi
 fi
 echo "# HELP conntrack_exporter_version Check conntrack_exporter binary version"
 echo "# TYPE conntrack_exporter_version gauge"
@@ -74,8 +81,10 @@ BLACKBOX_VERSION_LATEST=0
 if [ -f /usr/local/bin/blackbox_exporter ]; then
 	BLACKBOX_VERSION=$(/usr/local/bin/blackbox_exporter --version | head -1| awk '{print $3}')
         if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
-	BLACKBOX_VERSION_LATEST=$(curl -s https://api.github.com/repos/prometheus/blackbox_exporter/releases/latest| grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
-        if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	if [ "$LATEST_CHECK" -eq "1" ] ; then
+		BLACKBOX_VERSION_LATEST=$(curl -s https://api.github.com/repos/prometheus/blackbox_exporter/releases/latest| grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
+		if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	fi
 fi
 echo "# HELP blackbox_exporter_version Check blackbox_exporter binary version"
 echo "# TYPE blackbox_exporter_version gauge"
@@ -90,8 +99,10 @@ FLUENTBIT_VERSION_LATEST=0
 if [ -f /opt/fluent-bit/bin/fluent-bit ]; then
 	FLUENTBIT_VERSION=$(/opt/fluent-bit/bin/fluent-bit --version | head -1| awk '{print $3}' | sed 's/v//')
         if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
-	FLUENTBIT_VERSION_LATEST=$(curl -s https://api.github.com/repos/fluent/fluent-bit/releases/latest| grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
-        if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	if [ "$LATEST_CHECK" -eq "1" ] ; then
+		FLUENTBIT_VERSION_LATEST=$(curl -s https://api.github.com/repos/fluent/fluent-bit/releases/latest| grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
+		if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	fi
 fi
 echo "# HELP fluentbit_exporter_version Check fluentbit_exporter binary version"
 echo "# TYPE fluentbit_exporter_version gauge"
@@ -106,8 +117,10 @@ CADVISOR_VERSION_LATEST=0
 if [ -f /opt/cadvisor/cadvisor ]; then
 	CADVISOR_VERSION=$(/opt/cadvisor/cadvisor --version | head -1| awk '{print $3}' | sed 's/v//')
         if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
-	CADVISOR_VERSION_LATEST=$(curl -s https://api.github.com/repos/google/cadvisor/releases/latest| grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
-        if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	if [ "$LATEST_CHECK" -eq "1" ] ; then
+		CADVISOR_VERSION_LATEST=$(curl -s https://api.github.com/repos/google/cadvisor/releases/latest| grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
+		if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	fi
 fi
 echo "# HELP cadvisor_exporter_version Check cadvisor_exporter binary version"
 echo "# TYPE cadvisor_exporter_version gauge"
@@ -122,8 +135,10 @@ CONSUL_VERSION_LATEST=0
 if [ -f /usr/bin/consul ]; then
 	CONSUL_VERSION=$(/usr/bin/consul --version | head -1| awk '{print $2}' | sed 's/v//')
         if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
-	CONSUL_VERSION_LATEST=$(curl -s https://api.github.com/repos/hashicorp/consul/releases/latest| grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
-        if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	if [ "$LATEST_CHECK" -eq "1" ] ; then
+		CONSUL_VERSION_LATEST=$(curl -s https://api.github.com/repos/hashicorp/consul/releases/latest| grep '"tag_name"' | tr -d '"' | awk '{print $NF}' | sed -r 's/[v,]//gi')
+		if [ "$?" -ne "0" ] ; then PROBLEM_COUNT=$((PROBLEM_COUNT + 1)); fi
+	fi
 fi
 echo "# HELP consul_version Check consul binary version"
 echo "# TYPE consul_version gauge"
