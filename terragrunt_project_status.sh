7f035eb751a6a592ad10f93336cc2d58788c308a
lotusnoir <lotus.noir@gmail.com>
2025-04-11 12:26:34 +0000

    add terragrunt_project_status.sh

---
+++ terragrunt_project_status.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+### UPDATE
+#PROJECT_PATH="/path/to/terragrunt/"
+#REMOVE_PATH_PREFIX="prefix/path/"
+
+#Retrieve all the directories with a terragrunt.hcl configuration
+ALL_TERRAGRUNT_PROJECTS_PATH=$(find $PROJECT_PATH -name "terragrunt.hcl" -not -path "*terragrunt-cache/*" -execdir pwd \;)
+
+function terragrunt_check_projects_status () {
+    ACTUAL_PATH=$(pwd)
+    echo "# HELP terragrunt_project_status check plan status on project, 0 = all ok, 1 error, 2 changes to apply"
+    echo "# TYPE terragrunt_project_status gauge"
+    for path in ${ALL_TERRAGRUNT_PROJECTS_PATH}; do
+        cd "${path}" || return
+        short_path="${path#*$REMOVE_PATH_PREFIX}"
+        terragrunt plan -detailed-exitcode > /dev/null 2>&1
+        CODE=$?
+        echo "terragrunt_project_status{path="$short_path"} $CODE"
+    done
+}
+
+terragrunt_check_projects_status
