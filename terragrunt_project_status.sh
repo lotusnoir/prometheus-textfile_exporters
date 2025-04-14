0775cbb16cd85724c00705fd84973f4268853cb7
lotusnoir <lotus.noir@gmail.com>
2025-04-14 07:44:19 +0000

    add parralellisn to terragrunt_project_status.sh to speed up scrape

--- terragrunt_project_status.sh
+++ terragrunt_project_status.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-### UPDATE
-#PROJECT_PATH="/path/to/terragrunt/"
-#REMOVE_PATH_PREFIX="prefix/path/"
+### ANSIBLE BLOCK
+PROJECT_PATH="/opt/terraform_core/deploy/vsphere/vms"
+REMOVE_PATH_PREFIX="/deploy/vsphere/"
+### ANSIBLE BLOCK
 
-#Retrieve all the directories with a terragrunt.hcl configuration
-ALL_TERRAGRUNT_PROJECTS_PATH=$(find $PROJECT_PATH -name "terragrunt.hcl" -not -path "*terragrunt-cache/*" -execdir pwd \;)
+# Retrieve all the directories with a terragrunt.hcl configuration
+ALL_TERRAGRUNT_PROJECTS_PATH=$(find "$PROJECT_PATH" -name "terragrunt.hcl" -not -path "*terragrunt-cache/*" -execdir pwd \;)
 
-function terragrunt_check_projects_status () {
-    ACTUAL_PATH=$(pwd)
-    echo "# HELP terragrunt_project_status check plan status on project, 0 = all ok, 1 error, 2 changes to apply"
-    echo "# TYPE terragrunt_project_status gauge"
-    for path in ${ALL_TERRAGRUNT_PROJECTS_PATH}; do
-        cd "${path}" || return
-        short_path="${path#*$REMOVE_PATH_PREFIX}"
-        terragrunt plan -input=false -detailed-exitcode > /dev/null 2>&1
-        CODE=$?
-        echo "terragrunt_project_status{path=\"$short_path\"} $CODE"
-    done
-}
+# Prometheus headers
+echo "# HELP terragrunt_project_status check plan status on project, 0 = all ok, 1 = error, 2 = changes to apply"
+echo "# TYPE terragrunt_project_status gauge"
 
-terragrunt_check_projects_status
+# Export REMOVE_PATH_PREFIX to be used inside xargs commands
+export REMOVE_PATH_PREFIX
+
+# Run terragrunt checks in parallel
+printf "%s\n" $ALL_TERRAGRUNT_PROJECTS_PATH | xargs -P 8 -I{} bash -c '
+    path="{}"
+    short_path="${path#${REMOVE_PATH_PREFIX}}"
+    cd "$path" || exit 1
+    terragrunt plan -input=false -detailed-exitcode > /dev/null 2>&1
+    code=$?
+    echo "terragrunt_project_status{path=\"$short_path\"} $code"
+'
